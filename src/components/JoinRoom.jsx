e3fd2fb5f1b532a7c2f78dfba340556da7502f35
jatinbedii <jbedi432@gmail.com>
2024-01-29 22:08:33 +0530

    added name

--- src/components/JoinRoom.jsx
+++ src/components/JoinRoom.jsx
@@ -4,14 +4,16 @@ import React, { useState } from "react";
 
 function JoinRoom() {
   const [room, setroom] = useState("");
+  const [name, setname] = useState("");
   const router = useRouter();
   function joinhandler() {
-    router.push(`/room/${room}`);
+    router.push(`/room/${room}/${name}`);
   }
   return (
     <div>
       <div>
         <input value={room} onChange={(e) => setroom(e.target.value)} />
+        <input value={name} onChange={(e) => setname(e.target.value)} />
         <button onClick={joinhandler}>Join</button>
       </div>
     </div>
