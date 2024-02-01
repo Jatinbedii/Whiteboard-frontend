fdb70fe11d3ece8356b08a3683277f1e600ddb42
jatinbedii <jbedi432@gmail.com>
2024-02-01 21:28:59 +0530

    styled frontend of Join Screen

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,25 @@
 import JoinRoom from "@/components/JoinRoom";
 export default function Home() {
   return (
-    <main>
-      <JoinRoom />
+    <main className="min-h-screen bg-blue-500">
+      <div className="w-full bg-blue-500 flex justify-center">
+        <div className="flex flex-col">
+          <h1 className="text-black pt-2 text-3xl text-center">
+            W H I T E B O A R D
+          </h1>
+          <div className="w-full text-center">
+            <h2 className="text-black text-2xl p-2">CHAT ROOM</h2>{" "}
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-center">
+        <JoinRoom />
+      </div>
+
+      <div className="w-full text-center mt-[90px]">
+        Made with &lt;3 by{" "}
+        <span className="text-white font-medium">Jatin Bedi</span>
+      </div>
     </main>
   );
 }
--- src/components/JoinRoom.jsx
+++ src/components/JoinRoom.jsx
@@ -4,17 +4,44 @@ import React, { useState } from "react";
 
 function JoinRoom() {
   const [room, setroom] = useState("");
+  const [error, seterror] = useState("");
   const [name, setname] = useState("");
   const router = useRouter();
   function joinhandler() {
+    seterror("");
+    if (!room || !name) {
+      seterror("Please Fill all the Fields");
+      return;
+    }
     router.push(`/room/${room}/${name}`);
   }
   return (
-    <div>
-      <div>
-        <input value={room} onChange={(e) => setroom(e.target.value)} />
-        <input value={name} onChange={(e) => setname(e.target.value)} />
-        <button onClick={joinhandler}>Join</button>
+    <div className="max-w-fit p-5 bg-blue-700 rounded-md m-2">
+      <div className="flex flex-col gap-3 ">
+        <div className="text-white font-medium text-center">
+          Create a new Room or Join existing Room
+        </div>
+        <input
+          className="rounded"
+          placeholder="Enter Room Name"
+          value={room}
+          onChange={(e) => setroom(e.target.value)}
+        />
+        <input
+          className="rounded"
+          placeholder="Enter Your Name"
+          value={name}
+          onChange={(e) => setname(e.target.value)}
+        />
+        <div className="bg-red-600 pl-2 text-white rounded-md">{error}</div>
+        <div className="w-full text-center">
+          <button
+            onClick={joinhandler}
+            className="text-white font-semibold bg-blue-950 w-[50px] rounded-lg"
+          >
+            Join
+          </button>
+        </div>
       </div>
     </div>
   );
