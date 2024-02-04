a3919dba9b864cd49ad4ffe8dc31c5415825e645
jatinbedii <jbedi432@gmail.com>
2024-02-04 18:49:43 +0530

    UI improved

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import JoinRoom from "@/components/JoinRoom";
+import Image from "next/image";
 export default function Home() {
   return (
     <main className="min-h-screen bg-blue-500">
@@ -7,20 +8,32 @@ export default function Home() {
           <h1 className="text-black pt-2 text-3xl text-center md:text-6xl md:p-4">
             W H I T E B O A R D
           </h1>
-          <div className="w-full text-center">
+          <div className="w-full text-center font-normal">
             <h2 className="text-black text-2xl p-2 md:text-4xl md:p-3">
               CHAT ROOM
             </h2>{" "}
           </div>
         </div>
       </div>
+      <div className="flex justify-center mt-3 mb-5 p-2">
+        <Image src={"call.svg"} alt="logo" height={300} width={300} />
+      </div>
       <div className="flex justify-center">
         <JoinRoom />
       </div>
 
-      <div className="w-full text-center mt-[90px] md:text-xl pt-10">
-        Made with &lt;3 by
-        <span className="text-white font-medium"> Jatin Bedi</span>
+      <div
+        className="w-full text-white
+       text-center mt-[90px]  pt-10 font-medium text-sm"
+      >
+        Made with &lt;3 by <div></div>
+        <a
+          href="https://github.com/Jatinbedii"
+          target="_blank"
+          className="text-white font-medium"
+        >
+          Jatin Bedi
+        </a>
       </div>
     </main>
   );
--- src/components/JoinRoom.jsx
+++ src/components/JoinRoom.jsx
@@ -1,29 +1,33 @@
 "use client";
 import { useRouter } from "next/navigation";
 import React, { useState } from "react";
-
+import ClipLoader from "react-spinners/ClipLoader";
 function JoinRoom() {
+  const [loading, setloading] = useState(false);
   const [room, setroom] = useState("");
   const [error, seterror] = useState("");
   const [name, setname] = useState("");
   const router = useRouter();
   function joinhandler() {
+    setloading(true);
     seterror("");
     if (!room || !name) {
       seterror("Please Fill all the Fields");
+      setloading(false);
       return;
     }
+
     router.push(`/room/${room}/${name}`);
   }
   return (
     <div className="max-w-fit p-5 bg-blue-700 rounded-md m-2">
-      <div className="flex flex-col gap-3 ">
+      <div className="flex flex-col gap-3 w-full">
         <div className="text-white font-medium text-center md:text-2xl">
           Create a new Room or Join existing Room
         </div>
         <div className="w-full text-center">
           <input
-            className="rounded max-w-[300px] text-center"
+            className="rounded-2xl max-w-[300px] text-center"
             placeholder="Enter Room Name"
             value={room}
             onChange={(e) => setroom(e.target.value)}
@@ -31,20 +35,40 @@ function JoinRoom() {
         </div>
         <div className="w-full text-center">
           <input
-            className="rounded max-w-[300px] text-center"
+            className="rounded-2xl max-w-[300px] text-center"
             placeholder="Enter Your Name"
             value={name}
             onChange={(e) => setname(e.target.value)}
           />
         </div>
-        <div className="bg-red-600 pl-2 text-white rounded-md">{error}</div>
+        <div className="w-full">
+          {error ? (
+            <div className="text-red-900 p-1 m-1 w-fit bg-red-400 rounded-md mx-auto">
+              {error}
+            </div>
+          ) : (
+            <div></div>
+          )}
+        </div>
         <div className="w-full text-center">
-          <button
-            onClick={joinhandler}
-            className="text-white font-semibold bg-blue-950 w-[50px] rounded-lg"
-          >
-            Join
-          </button>
+          {loading ? (
+            <div>
+              <ClipLoader
+                color={"#ffffff"}
+                loading={loading}
+                size={30}
+                aria-label="Loading Spinner"
+                data-testid="loader"
+              />
+            </div>
+          ) : (
+            <button
+              onClick={joinhandler}
+              className="text-white font-semibold bg-blue-950 rounded-xl pt-1 pb-1 pl-5 pr-5"
+            >
+              Join
+            </button>
+          )}
         </div>
       </div>
     </div>
